f6f04aa51c0b3228d58a4321c249b03430162db0
Luke Tweedy <luke.tweedy@gmail.com>
2023-03-22 18:08:48 +0000

    Commited files at time  publication

---
+++ src/ThisClassIsNotReal.java
@@ -0,0 +1,10 @@
+public class ThisClassIsNotReal {
+
+    public static int NotReallyAThing;
+    public void DoesntDoAnything(){
+        int i=4;
+        int j=9;
+
+        NotReallyAThing = i+j;
+    }
+}
